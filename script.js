62995bb9d547e60d2da0c53ad5f2a2d525279214
Ale-1407 <alessiacarrera07@gmail.com>
2025-11-02 12:05:21 +0100

    primo modulo

---
+++ script.js
@@ -0,0 +1,9 @@
+// esportare un oggetto chiamato student il quale avrà 3 proprietà: nome, cognome e corso
+
+const student = {
+    name: 'Federico',
+    cognome: 'Rossi',
+    corso: 'Specializzazione React'
+}
+
+export default student;
